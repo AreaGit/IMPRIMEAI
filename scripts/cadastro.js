4a9815c443aaf3365245ee6b1aef0d5e0457e623
gabriel-trindade-15 <gabrieltrindade2606@gmail.com>
2023-08-27 16:30:35 -0300

    Autenticação quase feita

---
+++ scripts/cadastro.js
@@ -0,0 +1,35 @@
+function entrar() {
+    let email = document.getElementById('email');
+    let pass = document.getElementById('pass');
+    let msg = document.getElementById('msg');
+    let listaUser = [];
+    let userValid = {
+        email: '',
+        pass: ''
+    };
+
+    listaUser = JSON.parse(localStorage.getItem('listaUser'));
+
+    listaUser.forEach((item) => {
+        if (email.value === item.emailCad && pass.value === item.passCad) {
+            userValid = {
+                email: item.emailCad,
+                pass: item.passCad
+            };
+        }
+    });
+
+    if(email.value == userValid.email && pass.value == userValid.pass) {
+        //window.location.href = '../index.html'
+
+        let token = Math.random().toString(16).substring(2)
+        localStorage.setItem('token', token)
+        localStorage.setItem('userLogado', JSON.stringify(userValid))
+    } else {
+        email.setAttribute('style', 'color:red')
+        pass.setAttribute('style', 'color:red')
+        msg.setAttribute('style', 'color: red')
+        msg.innerHTML = '<strong>Usuário ou senha incorretos</strong>'
+        email.focus()
+    }
+}
