34307330a5d21a446f1d6d897c3360651e195069
AreaGit <areagit5@gmail.com>
2023-08-30 13:51:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -9,18 +9,6 @@ app.get("/", async (req,res) => {
 
 app.post("/cadastrar", async (req,res) => {
     console.log(req.body)
-    /*await User.create(req.body)
-    .then(() => {
-        return res.json({
-            erro: false,
-            mensagem: "Usuário cadastrado com sucesso!"
-        })
-    }).catch(() => {
-        return res.status(400).json({
-            erro: true,
-            mensagem: "Erro: Usuário não cadastrado com sucesso!"
-        })
-    })*/
     res.send("Página Cadastrar")
 })
 
