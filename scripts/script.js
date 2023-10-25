aec8e180534fbe3125012c66d21c97f448653278
AreaGit <areagit5@gmail.com>
2023-10-25 13:58:00 -0300

    Caixa de Pesquisa

--- scripts/script.js
+++ scripts/script.js
@@ -2,9 +2,67 @@ document.addEventListener('DOMContentLoaded', () => {
   const formPesquisa = document.getElementById('formPesquisa');
   const caixaPesq = document.getElementById('caixaPesq');
   const resultadosPesquisa = document.getElementById('resultadosPesquisa');
+  const limparPesq = document.getElementById('limparPesq');
+  // Variável para armazenar o ID do timer de pesquisa
+  let searchTimer;
+
+  formPesquisa.addEventListener('input', async () => {
+    const query = caixaPesq.value.trim();
+
+    if (query === '') {
+      resultadosPesquisa.style.display = 'none';
+      resultadosPesquisa.innerHTML = '';
+      return;
+    }
+
+    // Cancelar pesquisas anteriores (se houver alguma)
+    if (searchTimer) {
+      clearTimeout(searchTimer);
+    }
+
+    // Agendar uma nova pesquisa após um pequeno atraso (por exemplo, 300ms)
+    searchTimer = setTimeout(async () => {
+      try {
+        const response = await fetch(`/pesquisar-produtos?query=${encodeURIComponent(query)}`);
+        const data = await response.json();
+
+        resultadosPesquisa.innerHTML = '';
+
+        if (data.produtos && data.produtos.length === 0) {
+          resultadosPesquisa.innerHTML = '<p>Nenhum produto encontrado.</p>';
+        } else if (data.produtos) {
+          data.produtos.forEach((produto) => {
+            const cardProduto = document.createElement('a');
+            cardProduto.setAttribute('style', 'text-decoration:none; color:black;')
+            cardProduto.href = `html/detalhes-produtos.html?id=${produto.id}`;
+            cardProduto.classList.add('product-card');
+  
+            cardProduto.innerHTML = `
+              <img src="/imagens/${produto.id}" alt="${produto.nomeProd}">
+              <h2>${produto.nomeProd}</h2>
+              <!--<p>${produto.descProd}</p>
+              <p>Valor: R$ ${parseFloat(produto.valorProd).toFixed(2)}</p>-->
+            `;
+  
+            resultadosPesquisa.appendChild(cardProduto);
+          });
+          resultadosPesquisa.style.display = 'block';
+        }
+      } catch (error) {
+        console.error('Erro na pesquisa de produtos:', error);
+        resultadosPesquisa.innerHTML = '<p>Erro na pesquisa de produtos.</p>';
+      }
+    }, 300); // Defina o tempo de atraso desejado (em milissegundos)
+  });
+});
+
+
+/*document.addEventListener('DOMContentLoaded', () => {
+  const formPesquisa = document.getElementById('formPesquisa');
+  const caixaPesq = document.getElementById('caixaPesq');
+  const resultadosPesquisa = document.getElementById('resultadosPesquisa');
   const limparPesq = document.getElementById('limparPesq')
   
-
 
   formPesquisa.addEventListener('submit', async (event) => {
     event.preventDefault();
@@ -31,7 +89,8 @@ document.addEventListener('DOMContentLoaded', () => {
         resultadosPesquisa.innerHTML = '<p>Nenhum produto encontrado.</p>';
       } else if (data.produtos) {
         data.produtos.forEach((produto) => {
-          const cardProduto = document.createElement('div');
+          const cardProduto = document.createElement('a');
+          cardProduto.href = `html/detalhes-produtos.html?id=${produto.id}`;
           cardProduto.classList.add('product-card');
 
           cardProduto.innerHTML = `
@@ -50,7 +109,7 @@ document.addEventListener('DOMContentLoaded', () => {
       resultadosPesquisa.innerHTML = '<p>Erro na pesquisa de produtos.</p>';
     }
   });
-});
+});*/
 btnPesq.addEventListener('click', () => {
                 const query = caixaPesq.value.trim();
                 if (query !== '') {
