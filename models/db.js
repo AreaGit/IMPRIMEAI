fb3567e1732f04c61fa6993ebaeb40a684dfd27a
AreaGit <areagit5@gmail.com>
2024-02-07 11:51:12 -0300

    Duas Bases de Dados

--- models/db.js
+++ models/db.js
@@ -3,8 +3,18 @@ const DataTypes = require('sequelize')
 
 
 
-const sequelize = new Sequelize("infoimprimeai", "infoimprimeai", "admBancoD@dos2", {
+/*const sequelize = new Sequelize("infoimprimeai", "infoimprimeai", "admBancoD@dos2", {
     host: "infoimprimeai.mysql.dbaas.com.br",
+    dialect: 'mysql'
+})
+sequelize.authenticate()
+.then(function () {
+    console.log("Conectado ao banco de dados com sucesso!")
+}).catch(function() {
+    console.log("Erro ao conectar com o banco de dados")
+})*/
+const sequelize = new Sequelize("listauser", "root", "", {
+    host: "localhost",
     dialect: 'mysql'
 })
 sequelize.authenticate()
